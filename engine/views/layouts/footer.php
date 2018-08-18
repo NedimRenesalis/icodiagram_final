b73f40f9c1847e52ebc97b55d36beac3a9aa934a
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2018-08-18 12:34:37 +0200

    Removed label from subscribe pane

--- engine/views/layouts/footer.php
+++ engine/views/layouts/footer.php
@@ -49,7 +49,6 @@
                     ],
                 ]);?>
 
-                <label><?=t('app', 'Subscribe')?></label>
                 <div class="row">
                     <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
                         <?=$form->field($subscribeModel, 'email', [
